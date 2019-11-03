1a98f940b2423381395c9c27333e1ea8e8114990
Nazim Uddin <umnazim@gmail.com>
2019-11-03 12:38:40 -0800

    react app updated

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClickyGame from './components/ClickyGame';
+import './index.css';
+
+ReactDOM.render(<ClickyGame />, document.getElementById('root'));
+
+
