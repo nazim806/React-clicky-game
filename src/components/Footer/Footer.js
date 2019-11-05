13d207c1f705de6b00cf7fa965bde24922db50ed
Nazim Uddin <umnazim@gmail.com>
2019-11-04 22:30:40 -0800

    initial commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => <div className="footer text-center">Nazim Uddin</div>;
+const Footer = () => <div className="footer text-center">2019 github</div>;
 
 export default Footer;
